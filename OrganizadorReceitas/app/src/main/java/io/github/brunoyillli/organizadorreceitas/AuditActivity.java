f9f9190e6b4d913c289486b1371d95ed5494d59d
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-06-03 23:18:17 -0300

    estudos

---
+++ OrganizadorReceitas/app/src/main/java/io/github/brunoyillli/organizadorreceitas/AuditActivity.java
@@ -0,0 +1,14 @@
+package io.github.brunoyillli.organizadorreceitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audit);
+    }
+}
